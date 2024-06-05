621b2b44bf3fbd53adb49966ebc60620926ac70f
Lexuzops <john.builes.20001913@hotmail.com>
2024-06-05 19:54:40 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,8 +1,13 @@
 function skillMember() {
-  var member = {
-    name: 'John',
-    age: 25,
-    skill: 'JavaScript'
-  };
-  return member;
+    var member = {
+        name: 'John',
+        age: 30
+    };
+    this.getMember = function(){
+        return member;
+    };
+    this.setMember = function(name, age){
+        member.name = name;
+        member.age = age;
+    };
 }
